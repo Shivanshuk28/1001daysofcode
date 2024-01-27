3982e4a788c3b023e4823a98ef867619d8d18396
shivanshuk28 <2contactshivanshu@gmail.com>
2024-01-27 23:12:44 +0530

    day 260 added

---
+++ day 260/1.cpp
@@ -0,0 +1,32 @@
+// Leetcode:
+// 82. Remove Duplicates from Sorted List II
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+         map<int,int>mpp;
+         ListNode* temp=head;
+         while(temp){
+             mpp[temp->val]++;
+             temp=temp->next;   
+         }
+         ListNode* temp2=new ListNode(0);
+         ListNode* head2=temp2;
+         for(auto x:mpp){
+             pif(x.second==1){
+                 temp2->next=new ListNode(x.first);
+                 temp2=temp2->next;
+             }
+         }
+         return head2->next;
+    }
+};
